48b6d53243bcf2fd6a323ce9051ec97014654a77
Owner <owner@owners-macbook-pro.local>
2019-03-16 15:07:08 -0400

    Initial Commit

---
+++ Login_plist/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Login_plist
+//
+//  Created by Owner on 2019-03-15.
+//  Copyright © 2019 Owner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+var user:String?
+    var pass:String?
+    @IBOutlet weak var txtpass: UITextField!
+    @IBOutlet weak var txtuid: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func btnsignin(_ sender: UIButton) {
+        readInformationPlist()
+        
+    }
+    
+    func readInformationPlist() {
+        
+       if let bundlePath = Bundle.main.path(forResource: "Login", ofType: "plist") {
+            let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+        
+             let employeeList = dictionary!["Employee"] as! NSArray
+            for e in employeeList
+            {
+                let emp = e as! NSDictionary
+                self.user=(emp["uid"]! as! String)
+                self.pass=(emp["pass"]! as! String)
+        
+                if(user==txtuid.text && pass==txtpass.text)
+                {
+                    print("login sucess")
+                    print("uid::\(self.user!)")
+                    print("Pass::\(self.pass!)")
+                }
+                else
+                {
+                    print("login fail")
+                    print("uid::\(self.user!)")
+                    print("Pass::\(self.pass!)")
+                    print("----------------")
+                    print(txtuid.text)
+                    print(txtpass.text)
+                }
+            }
+            
+            }
+            
+        }
+    }
+
+
